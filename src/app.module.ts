ebb25a8ec673109b9a73d9e214d89faed67d3d6c
Kubilay <kubilaysabah@gmail.com>
2025-03-23 00:32:45 +0300

    feat(auth): create auth module
    -add auth module
    -add nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from '@app.controller';
 import { AppService } from '@app.service';
 
+import { AuthModule } from '@modules/auth/auth.module';
+
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.env.development.local',
+        '.env.development',
+        '.env.production',
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
