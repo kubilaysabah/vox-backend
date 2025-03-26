07a90fa40f92533894fefe5c6f3a269f8c5049d7
Kubilay <kubilaysabah@gmail.com>
2025-03-26 23:39:48 +0300

    reinitialize vox

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
-import { AppController } from '@app.controller';
-import { AppService } from '@app.service';
-
-import { AuthModule } from '@modules/auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    AuthModule,
-    ConfigModule.forRoot({
-      envFilePath: [
-        '.env.development.local',
-        '.env.development',
-        '.env.production',
-      ],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {PrismaClient} from '@prisma/client'
 
-void (async (): Promise<void> => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+import {AppModule} from './app.module';
+
+const prisma = new PrismaClient()
+
+async function connectDatabase() {
+  try {
+      await prisma.$disconnect()
+  } catch (error) {
+      console.error(error)
+      await prisma.$disconnect()
+      process.exit(1)
+  }
+}
+
+(async () => {
+    const app = await NestFactory.create(AppModule);
+    await connectDatabase();
+    await app.listen(process.env.PORT ?? 3000);
 })();
