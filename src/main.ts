6d67028a6bbddef43216bbe12e82065a60b760a9
Kubilay <kubilaysabah@gmail.com>
2025-03-27 10:53:53 +0300

    add prisma generators

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import {NestFactory} from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import {PrismaClient} from '@prisma/client'
 
 import {AppModule} from './app.module';
@@ -7,9 +9,10 @@ const prisma = new PrismaClient()
 
 async function connectDatabase() {
   try {
+      console.log('Connected database...');
       await prisma.$disconnect()
   } catch (error) {
-      console.error(error)
+      console.error('Error connecting to database: ', error)
       await prisma.$disconnect()
       process.exit(1)
   }
@@ -17,6 +20,14 @@ async function connectDatabase() {
 
 (async () => {
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
     await connectDatabase();
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 5000);
 })();
